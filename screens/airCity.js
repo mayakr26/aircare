3993dc35b2f9f298dae6bf7f087f41f65de1d829
Maya Kröger <maya.kroeger@haw-hamburg.de>
2021-06-16 20:25:47 +0200

    airCity table

--- screens/airCity.js
+++ screens/airCity.js
@@ -1,36 +1,54 @@
-import React, {useLayoutEffect, useContext} from 'react';
-import {StyleSheet, View, Text} from 'react-native';
-import {Button} from 'react-native-elements';
-import {Ionicons} from '@expo/vector-icons';
+import React, { useLayoutEffect, useContext, useState } from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+import { Button } from 'react-native-elements';
+import { Ionicons } from '@expo/vector-icons';
 import { ToDoContext } from '../data/ToDoContext';
-
-export default AirCity = ({route, navigation}) => {
-  
-    const {itemId} = route.params;
-
-    const [toDoData, setToDoData] = useContext(ToDoContext);
-    const selectedCity = toDoData.cities.find(cit => cit.id === itemId);
-    const cityName = selectedCity.name;
-
-    useLayoutEffect(() => {
-        navigation.setOptions({      
-            headerTitle: cityName
-          });
-    }, [navigation]);
+import { Table, TableWrapper, Row, Rows, Col, Cols, Cell } from 'react-native-table-component';
 
 
-    return (
-        <View style={styles.container}>
-          <Text>City Air Condition here!</Text>
-        </View>
-    )
+export default AirCity = ({ route, navigation }) => {
+
+  const { itemId } = route.params;
+
+  const [getABC, setABC] = useState({
+    tableHead: ['Head', 'Head2', 'Head3', 'Head4'],
+    tableData: [
+      ['1', '2', '3', '4'],
+      ['a', 'b', 'c', 'd'],
+      ['1', '2', '3', '456\n789'],
+      ['a', 'b', 'c', 'd']
+    ]
+  })
+
+  const [toDoData, setToDoData] = useContext(ToDoContext);
+  const selectedCity = toDoData.cities.find(cit => cit.id === itemId);
+  const cityName = selectedCity.name;
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      headerTitle: cityName
+    });
+  }, [navigation]);
+
+
+  return (
+    <View style={styles.container}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getABC.tableHead} style={styles.head} textStyle={styles.text} />
+        <Rows data={getABC.tableData} textStyle={styles.text} />
+      </Table>
+    </View>
+  )
 };
 
+
 const styles = StyleSheet.create({
-    container: {
-      flex: 1,
-      backgroundColor: '#fff',
-      alignItems: 'center',
-      justifyContent: 'center',
-    },
-  });
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'flex-start',
+  },
+
+  head: { height: 40, backgroundColor: '#f1f8ff' },
+  text: { margin: 6 }
+});
