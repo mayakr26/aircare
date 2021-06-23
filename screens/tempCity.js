ceed60435c07f45a1e86684fdd799db4566e69fa
Maya Kröger <maya.kroeger@haw-hamburg.de>
2021-06-23 21:42:14 +0200

    graph for each city and month

--- screens/tempCity.js
+++ screens/tempCity.js
@@ -3,7 +3,7 @@ import { StyleSheet, View, Text, useWindowDimensions } from 'react-native';
 import { ToDoContext } from '../data/ToDoContext';
 import { Table, Row, Rows } from 'react-native-table-component';
 import { TabView, SceneMap, TabBar } from 'react-native-tab-view';
-
+import { Chart, VerticalAxis, HorizontalAxis, Line } from 'react-native-responsive-linechart';
 
 
 export default TempCity = ({ route, navigation }) => {
@@ -17,7 +17,19 @@ export default TempCity = ({ route, navigation }) => {
 
   const layout = useWindowDimensions();
 
-  let times = [1961, 1971, 1981, 1991, 2001, 2011, 2021]
+  let times = [1961, 1971, 1981, 1991, 2001, 2011, 2021];
+
+
+  let data1 = [[], [], [], [], [], [], [], [], [], [], [], []];
+
+  for (let a = 0; a < 12; a += 1) {
+    for (let i = 0; i < times.length; i += 1) {
+      data1[a].push({
+        x: times[i],
+        y: selectedCity.temps.month[a][i]
+      })
+    }
+  }
 
   const rowData = [[], [], [], [], [], [], [], [], [], [], [], []];
   for (let a = 0; a < 12; a += 1) {
@@ -39,83 +51,227 @@ export default TempCity = ({ route, navigation }) => {
   }, [navigation]);  
 
   const FirstRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[0]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[0]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[0]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[0]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[0]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const SecondRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[1]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[1]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[1]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[1]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[1]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const ThirdRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[2]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[2]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[2]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[2]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[2]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
   const FourthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[3]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[3]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[3]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[3]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[3]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const FifthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[4]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[4]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[4]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[4]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[4]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
   const SixthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[5]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[5]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[5]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[5]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[5]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const SeventhRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[6]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[6]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[6]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[6]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[6]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const EighthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[7]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[7]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[7]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[7]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[7]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
   const NinthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[8]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[8]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table  borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[8]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[8]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[8]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
   const TenthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[9]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[9]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[9]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[9]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[9]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const EleventhRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[10]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[10]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[10]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[10]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[10]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   const TwelfthRoute = () => (
-    <Table style={styles.table} borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
-      <Row data={getData.tableHead[11]} style={styles.head} textStyle={styles.text} />
-      <Rows data={getData.tableData[11]} textStyle={styles.text} />
-    </Table>
+    <View style={styles.view}>
+      <Table borderStyle={{ borderWidth: 1, borderColor: '#c8e1ff' }}>
+        <Row data={getData.tableHead[11]} style={styles.head} textStyle={styles.text} />
+        <Rows data={getData.tableData[11]} textStyle={styles.text} />
+      </Table>
+      <Chart
+      style={{ height: 120, width: '100%', marginTop: 20 }}
+      xDomain={{ min: 1961, max: 2021 }}
+      yDomain={{ min: -10, max: 40 }}
+      padding={{ left: 20, top: 10, bottom: 20, right: 10 }}
+     >
+      <VerticalAxis tickValues={[-10, 0, 10, 20, 30, 40]} />
+      <HorizontalAxis tickCount={7} />
+      <Line data={data1[11]} smoothing="none" theme={{ stroke: { color: '#0068d3', width: 2 } }} />
+      </Chart>
+    </View>
   );
 
   
@@ -186,7 +342,7 @@ const styles = StyleSheet.create({
     backgroundColor: '#fff',
     justifyContent: 'flex-start',
   },
-  table: {
+  view: {
     margin: 30,
   },
   tableTop: {
